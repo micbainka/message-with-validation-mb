9129af1418317e53df9807daa877e353fe98501f
Michal <michal.bainka@bayer.com>
2025-01-06 12:07:16 +0100

    Added Error Message on textarea

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [searchMessage, setSearchMessage] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [noteValue, setNoteValue] = useState("");
+  const characterLimit = 30;
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(searchMessage);
+  const handleChange = (event) => {
+    setNoteValue(event.target.value);
+    setShowError(event.target.value.length > characterLimit);
   };
 
   return (
     <div className="App">
-Text
+      <textarea
+        value={noteValue}
+        onChange={handleChange}
+        placeholder="Insert message"
+      />
+      {showError ? <div>Inserted text is too long!</div> : null}
     </div>
   );
 }
